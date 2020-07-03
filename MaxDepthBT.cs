25cfb7b77ee191da6ac7f77c221526e324504366
Arturo Filio Villa <arturofiliovilla@Arturos-MacBook-Pro.local>
2020-07-03 03:50:31 -0400

    added a solution to the find max depth on a binary tree

--- MaxDepthBT.cs
+++ MaxDepthBT.cs
@@ -1,23 +1,34 @@
+using System;
+
 namespace algorithms {
 
-  public class MaxDepthBT {
+  public class Node {
+    public int data;
+    public Node left, right;
 
-    // Method to Check the Max Depth;
-    public int MaxDepth(TreeNode root) {
-      return 0;    
+    public Node(int item) {
+      data = item;
+      left = right = null; // we initialize an empty tree
     }
-
   }
 
-  public class TreeNode {
-    public int val;
-    public TreeNode left;
-    public TreeNode right;
-    // Constructor
-    public TreeNode(int val= 0, TreeNode left = null, TreeNode right = null) {
-      this.val = val;
-      this.left = left;
-      this.right = right;
+  /**
+    - In this class we are creating the method max depth where we check 
+    each Node and it's child Nodes
+    - We know we have reached the max height when a node has a null child
+    - The max height is determined by checking whether one Node is bigger than the other as shown here
+    - IN order to check the Max Depth we use a recursive method
+  */
+  
+  public class BinaryTree {
+    public Node root;
+    
+    public int maxDepth(Node node) {
+      if (node == null)
+        return 0;
+      int left = maxDepth(node.left);
+      int right = maxDepth(node.right);
+      return Math.Max(left,right);
     }
   }
     
